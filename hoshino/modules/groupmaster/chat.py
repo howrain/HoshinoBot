8fb7ef0cd47b926c8239d42bc93923bee38be9ce
howrain <842648152@qq.com>
2020-11-12 23:23:07 +0800

    add chat

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -13,6 +13,7 @@ async def say_hello(session):
 
 sv = Service('chat', visible=False)
 
+
 @sv.on_fullmatch('沙雕机器人')
 async def say_sorry(bot, ev):
     await bot.send(ev, 'ごめんなさい！嘤嘤嘤(〒︿〒)')
@@ -41,7 +42,7 @@ async def seina(bot, ev):
     await bot.send(ev, R.img('星奏.png').cqcode)
 
 
-@sv.on_fullmatch(('我有个朋友说他好了', '我朋友说他好了', ))
+@sv.on_fullmatch(('我有个朋友说他好了', '我朋友说他好了',))
 async def ddhaole(bot, ev):
     await bot.send(ev, '那个朋友是不是你弟弟？')
     await util.silence(ev, 30)
@@ -51,6 +52,13 @@ async def ddhaole(bot, ev):
 async def nihaole(bot, ev):
     await bot.send(ev, '不许好，憋回去！')
     await util.silence(ev, 30)
+
+
+@sv.on_rex(r'.*不够[色涩].*')
+async def bugouse(bot, ev):
+    if random.random() < 0.5:
+        await bot.send(ev, '那你发')
+        await util.silence(ev, 30)
 
 
 # ============================================ #
@@ -73,11 +81,13 @@ async def chat_neigui(bot, ctx):
     if random.random() < 0.10:
         await bot.send(ctx, R.img('内鬼.png').cqcode)
 
+
 nyb_player = f'''{R.img('newyearburst.gif').cqcode}
 正在播放：New Year Burst
 ──●━━━━ 1:05/1:30
 ⇆ ㅤ◁ ㅤㅤ❚❚ ㅤㅤ▷ ㅤ↻
 '''.strip()
+
 
 @sv.on_keyword(('春黑', '新黑'))
 async def new_year_burst(bot, ev):
