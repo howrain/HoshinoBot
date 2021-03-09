671f2b384f54c2c2ae69669adb3adc3d3aa1436e
howrain <842648152@qq.com>
2021-03-09 17:10:53 +0800

    修复退群提醒

--- hoshino/modules/groupmaster/group_notice.py
+++ hoshino/modules/groupmaster/group_notice.py
@@ -3,17 +3,20 @@ import hoshino
 from hoshino import Service
 from hoshino.typing import NoticeSession
 
-sv1 = Service('group-leave-notice', help_='退群通知')
+sv1 = Service('退群通知', help_='退群通知')
+@sv1.on_notice('group_decrease')
+async def decrease_leave(session: NoticeSession):
+    uid = session.event['user_id']
+    bot = hoshino.get_bot()
+    member=await bot.get_stranger_info(user_id=uid)
+    nickname=member['nickname']
+    msg = f'{nickname}({uid})退群了，期待再度相逢的那一天！'
+    await session.send(msg)
 
-@sv1.on_notice('group_decrease.leave')
-async def leave_notice(session: NoticeSession):
-    await session.send(f"{session.ctx['user_id']}退群了。")
-
-
-sv2 = Service('group-welcome', help_='入群欢迎')
+sv2 = Service('入群欢迎', help_='入群欢迎')
 
 @sv2.on_notice('group_increase')
-async def increace_welcome(session: NoticeSession):
+async def increase_welcome(session: NoticeSession):
     
     if session.event.user_id == session.event.self_id:
         return  # ignore myself
