2b777e667730c09c528fdf3460743bc4a77aacd4
howrain <842648152@qq.com>
2021-03-09 17:11:26 +0800

    修复邀请提醒

--- hoshino/modules/botmanage/group_invite.py
+++ hoshino/modules/botmanage/group_invite.py
@@ -8,4 +8,4 @@ async def handle_group_invite(session: RequestSession):
     if session.ctx['user_id'] in nonebot.get_bot().config.SUPERUSERS:
         await session.approve()
     else:
-        await session.reject(reason='邀请入群请联系维护组')
+        await session.reject(reason='需要超管权限，邀请入群请联系维护组')
